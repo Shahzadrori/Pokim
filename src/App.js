d4cf423376eb4c01232649f38a8666b32bc5f29d
Shahzad <shahzadrori@gmail.com>
2021-01-03 21:52:41 +0500

    updating index and App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Poki =() =>{
+let [num,setnum] = useState();
 
 
-    
+return (
+<React.Fragment>
+    <h1>You have choose {num}</h1>
+    <select
+    value={num}
+    onChange={(eve) =>{
+      setnum(eve.target.value);
+    }}
+    >
+        <option value='1'>1</option>
+        <option value='25'>25</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
+    </select>
+</React.Fragment>
+)
+
 }
 
 export default Poki;
