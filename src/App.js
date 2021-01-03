2d22266f277dbb180fd80d3345ebad57653a371f
Shahzad <shahzadrori@gmail.com>
2021-01-03 21:33:55 +0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Poki =() =>{
+
+
+    
+}
+
+export default Poki;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+  <>
+    <Poki />
+  </>,
+  document.getElementById('root')
+)
