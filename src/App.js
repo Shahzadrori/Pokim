a763b28af1a235568e3641b36444ec8257fd8e3e
Shahzad <shahzadrori@gmail.com>
2021-01-03 22:27:42 +0500

    Using useState for useEffect in async function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Poki =() =>{
 let [num,setnum] = useState();
+let [name,setname] = useState();
+let [moves,setmoves] = useState();
+
+useEffect(
+    async function getdata(){
+        const res = await axios.get(`${num}`);
+        setname(res.data.name);
+        setmoves(res.data.moves.length);
+    }
+);
 
 
 return (
 <React.Fragment>
-    <h1>You have choose {num}</h1>
+    <h1>You have choose <span style={{color:'red'}}>{num}</span></h1>
+    <h1>My name is <span style={{color:'red'}}>{name}</span></h1>
+    <h1>I have <span style={{color:'red'}}>{moves}</span> moves</h1>
     <select
     value={num}
     onChange={(eve) =>{
